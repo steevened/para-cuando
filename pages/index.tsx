470ecb671e5240839bb2b23a2e1d7d8b34d5fe5b
steven <alvaradosteven315@gmail.com>
2023-02-20 15:59:27 -0500

    build: create atoms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Arrow from '@/components/atoms/Arrow';
+import Eye from '@/components/atoms/Eye';
+import UserLogo from '@/components/atoms/UserLogo';
+import UserOutlined from '@/components/atoms/UserOutlined';
 import Button from '@/components/buttons/Button';
+import HearthBtn from '@/components/buttons/HearthBtn';
 import Layout from '@/components/layouts/Layout';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -24,7 +28,7 @@ const Home: NextPageWithLayout = () => {
         <title>Para Cuándo</title>
         <meta name="description" content="description" />
       </Head>
-      <div className="flex flex-col items-center justify-between py-10 text-white bg-app-blackLight">
+      <div className="flex flex-col items-center justify-between py-10 text-white bg-app-grayDark">
         <h1 className="title-1">HOME</h1>
         <ul className="flex justify-between w-full mt-10 sm:w-3/4">
           {categories.map((categorie) => (
@@ -40,7 +44,13 @@ const Home: NextPageWithLayout = () => {
             </li>
           ))}
         </ul>
-        <Arrow orientation="left" />
+        <div className="flex items-center justify-center gap-4 mt-10">
+          <Arrow orientation="left" />
+          <HearthBtn />
+          <UserLogo />
+          <UserOutlined />
+          <Eye />
+        </div>
       </div>
     </>
   );
