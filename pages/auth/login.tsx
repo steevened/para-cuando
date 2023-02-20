aab801d80155284d0e2514b5738eab30cad4dfd3
steven <alvaradosteven315@gmail.com>
2023-02-20 02:26:41 -0500

    build: styling login and sign up

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,3 +1,21 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
 export default function login() {
-  return <div>login</div>;
+  return (
+    <>
+      <Head>
+        <title>Log In - Para Cuándo</title>
+        <meta name="description" content="description" />
+      </Head>
+      <div className="flex flex-col items-center justify-start gap-10 bg-app-blackLight text-white h-screen  pt-10">
+        <Link href="/">
+          <button className="px-4 py-2 rounded-lg shadow-lg bg-slate-700 ">
+            Página principal
+          </button>
+        </Link>
+        <h1 className="title-1 ">login</h1>
+      </div>
+    </>
+  );
 }
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,3 +1,21 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
 export default function register() {
-  return <div>register</div>;
+  return (
+    <>
+      <Head>
+        <title>Sign Up - Para Cuándo</title>
+        <meta name="description" content="description" />
+      </Head>
+      <div className="flex flex-col items-center justify-start gap-10 bg-app-blackLight text-white h-screen  pt-10">
+        <Link href="/">
+          <button className="px-4 py-2 rounded-lg shadow-lg bg-slate-700 ">
+            Página principal
+          </button>
+        </Link>
+        <h1 className="title-1 ">Sign Up</h1>
+      </div>
+    </>
+  );
 }
