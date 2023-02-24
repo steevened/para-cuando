11df688a71c18ab5e16db3ec34ce521fb61125b3
steven <alvaradosteven315@gmail.com>
2023-02-24 15:37:31 -0500

    build: style components

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -7,7 +7,7 @@ interface SectionProps {
 
 export default function Main() {
   return (
-    <div>
+    <div className="my-5">
       <Section
         title="Populares en Querétaro"
         subtitle="Lo que las personas piden más"
@@ -19,10 +19,10 @@ export default function Main() {
 
 function Section({ title, subtitle }: SectionProps) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{subtitle}</p>
-      <HomeSlider />
+    <div className="pl-5 max-w-[1103px] mx-auto mt-12">
+      <h2 className="title-2 mt-2.5">{title}</h2>
+      <p className="mt-2 subtitle-2 text-app-grayDark">{subtitle}</p>
+      <HomeSlider className="mt-9" />
     </div>
   );
 }
--- lib/interfaces/components.interface.ts
+++ lib/interfaces/components.interface.ts
@@ -1,3 +1,14 @@
+import { StaticImageData } from 'next/image';
+
 export default interface Children {
   children: React.ReactNode;
+}
+
+export interface ItemSlider {
+  id: number;
+  title: string;
+  description: string;
+  web: string;
+  assistants: number;
+  img: StaticImageData;
 }
