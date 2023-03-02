8fae9d528ba1fa83e5a45e328a39ad90506580a3
steven <alvaradosteven315@gmail.com>
2023-03-01 22:26:51 -0500

    build: create input component

---
+++ components/create/Input.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from 'react';
+interface InterfaceInput extends React.ComponentPropsWithoutRef<'input'> {
+  label: string;
+}
+
+export const Input: React.FC<InterfaceInput> = React.forwardRef(
+  ({ label, className, ...inputProps }, _ref) => {
+    const refContainer = useRef<any>(null);
+    const [inputValue, setInputValue] = useState('');
+    useEffect(() => {
+      if (refContainer) {
+        setInputValue(refContainer.current.value);
+      }
+    }, [refContainer]);
+    return (
+      <label className="relative">
+        <input
+          ref={refContainer}
+          {...inputProps}
+          className={`w-full px-5 py-2 duration-100 border peer border-app-grayDark rounded-xl focus:ring-1 focus:ring-app-blue focus:outline-none ${className}`}
+        />
+        <span
+          className={`absolute text-base cursor-text text-app-grayDark peer-active:-top-[25px] whitespace-nowrap left-3 -top-[4px] bg-white p-1 duration-200 peer-focus:-top-[25px] peer-focus:text-sm ease-in-out ${
+            (inputValue || refContainer?.current?.value) &&
+            '-top-[25px] text-sm'
+          }`}
+        >
+          {label}
+        </span>
+      </label>
+    );
+  }
+);
+
+Input.displayName = 'Input';
