75f03c1c0b03a0bfa936daf29b5ce3477ea9fa17
steven <alvaradosteven315@gmail.com>
2023-06-26 15:42:43 -0500

    refactor event page

--- components/categories/homeCategories/CategorieNavbar.tsx
+++ components/categories/homeCategories/CategorieNavbar.tsx
@@ -1,18 +1,17 @@
 import Input from '@/components/Forms/Input';
-import { Types } from '@/lib/interfaces/publicationTypes/publicationTypes.interface';
 import { FC } from 'react';
 import { MenuDropDown } from './MenuDropdown';
 import PostCategories from './PostCategories';
 
-interface Props {
-  publicationTypes: Types;
-}
+// interface Props {
+//   publicationTypes: Types;
+// }
 
-const CategorieNavbar: FC<Props> = ({ publicationTypes }) => {
+const CategorieNavbar: FC = () => {
   return (
     <div className="relative w-full shadow-shadow1 py-9">
       <div className="flex px-5 max-w-[980px] mx-auto justify-between gap-2 items-center">
-        <MenuDropDown publicationTypes={publicationTypes} />
+        <MenuDropDown />
         <div className="hidden md:block md:flex-[1] mr-10">
           <PostCategories />
         </div>
