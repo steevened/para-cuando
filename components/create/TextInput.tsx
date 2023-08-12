727fce721b49b326af5dc8bc8000ec3af418410e
Steve <105613222+steevened@users.noreply.github.com>
2023-08-11 23:28:58 -0500

    Update TextInput.tsx transition duration

--- components/create/TextInput.tsx
+++ components/create/TextInput.tsx
@@ -13,7 +13,7 @@ const TextInput: FC<any> = ({ label, className, ...props }) => {
         <input
           {...field}
           {...props}
-          className={`w-full px-5 py-2 duration-100 border peer rounded-xl focus:ring-1 focus:ring-app-blue border-app-grayDark ${
+          className={`w-full px-5 py-2 duration-300 border peer rounded-xl focus:ring-1 focus:ring-app-blue border-app-grayDark ${
             meta.touched && meta.error
               ? 'border-red-500'
               : '  focus:outline-none border-app-blue'
